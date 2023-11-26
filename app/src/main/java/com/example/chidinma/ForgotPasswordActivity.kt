d296b9c158bdda32d01da402d37e566cfaf923c1
chidinma <onyejiuwa93@gmail.com>
2023-11-27 00:00:25 +0100

    Empty Username and Password for Assessment

--- app/src/main/java/com/example/chidinma/ForgotPasswordActivity.kt
+++ app/src/main/java/com/example/chidinma/ForgotPasswordActivity.kt
@@ -14,8 +14,18 @@ class ForgotPasswordActivity : AppCompatActivity() {
 
         var reset : Button =findViewById(R.id.ResetPassword)
         val email: EditText = findViewById(R.id.Emailaddress)
-        val nmaEmail : String = email.text.toString()
+
+
         reset.setOnClickListener{
+            val nmaEmail : String = email.text.toString()
+            var message ="";
+
+            if (!nmaEmail.matches(Regex("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\$"))){
+                message = "Enter an email address"
+            }else{
+                message = "Email has been sent"
+
+            }
             Snackbar.make(findViewById
                 (R.id.identity), " An Email Has Been Sent to $nmaEmail",
                 Snackbar.LENGTH_LONG).show()
